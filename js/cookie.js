99a4b654f41cdc0544c719ef27b223d6f3d90d1b
wikipedia05 <159623308+wikipedia05@users.noreply.github.com>
2024-05-22 10:53:16 +1000

    Update cookie.js

--- js/cookie.js
+++ js/cookie.js
@@ -22,3 +22,7 @@ function getCookie(name) {
 function eraseCookie(name) {   
     document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
+
+if (getCookie("token").length <=2){
+    setCookie("token","1234")
+}
